c1c21cea870718ede5d7d86107af62a5f8a2ba3c
Syam Narendra <syamnarendra85@gmail.com>
2023-12-20 12:06:12 +0530

    Sidebar key added in map

--- app/search-members/layout.js
+++ app/search-members/layout.js
@@ -4,27 +4,22 @@ import { usePathname } from "next/navigation";
 import "./styles.css";
 
 const sideBarButtons = [
-  {
-    link: "/search-members",
-    text: "Search Team",
-  },
-  {
-    link: "/search-members/saved-projects",
-    text: "Saved Projects",
-  },
+  { id: 1, link: "/search-members", text: "Search Team" },
+  { id: 2, link: "/search-members/saved-projects", text: "Saved Projects" },
 ];
 
 function SideBar({ children }) {
   const pathname = usePathname();
   console.log(pathname);
   return (
-    <div className="flex h-screen flex-col md:flex-row">
-      <div className="md:flex md:flex-col md:w-52 justify-center p-1 md:h-screen flex-row items-center w-screen h-min">
+    <div className="flex  flex-col md:flex-row">
+      <div className="md:flex md:flex-col md:w-52 justify-center p-1  flex-row items-center w-screen max-h-full">
         {sideBarButtons.map((each) => {
           const activeClass =
             each.link === pathname ? "text-white" : "text-gray-600";
           return (
             <Link
+              key={each.id}
               href={each.link}
               className={`${activeClass} w-full mb-5 font-bold py-2 px-4 rounded`}
             >
--- app/search-members/page.js
+++ app/search-members/page.js
@@ -2,7 +2,6 @@
 import "./styles.css";
 import MemberItem from "../../components/component/member-item.js";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@chakra-ui/react";
 const allMembers = [
@@ -24,19 +23,19 @@ export default function Members() {
           Project Name
         </Label>
         <div className="grid grid-cols-4 items-center gap-4">
-          <Input id="name" className="col-span-3" />
+          <input className="bg-black border border-white rounded-sm h-10 text-white focus:outline-none p-3" />
         </div>
         <Label htmlFor="username" className="text-left">
           Project Description
         </Label>
         <div className="grid grid-cols-4 items-center gap-4">
           <Textarea
-            className="col-span-3 h-40 bg-black text-white "
+            className="col-span-3 h-40 bg-black border rounded-md border-white text-white focus:outline-none p-3"
             placeholder="Describe Your Project"
           />
         </div>
       </div>
-      <Button className="border-white border-2 self-end">Search</Button>
+      <Button className="border-white border-2 self-start">Search</Button>
       <MemberItem />
       <Button className="border-white border-2 self-end">Save</Button>
     </div>
